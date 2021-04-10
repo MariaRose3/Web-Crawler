4475c0808b2b237528ec711f342ea61c03e8f6ae
MariaRose3 <mariapaliyam@gmail.com>
2021-04-10 12:05:15 -0500

    last commit

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import threading
 from queue import Queue
 from crawler import crawler
 from file_manage import *
-# from cli1 import *
+from cli1 import *
 
-DIRECTORY_NAME = "cyberlabs"
-HOME_PAGE = "http://cyberlabs.club"
-QUEUE_FILE = PROJECT_NAME + '/queue.txt'
-CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-NUMBER_OF_THREADS = 2
+DIRECTORY_NAME = args.output()                 #"cyberlabs"
+HOME_PAGE = args.url()                         #"http://cyberlabs.club"
+HEADERS = {"Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US,en;q=0.9", "Connection": "close"}
+QUEUE_FILE = DIRECTORY_NAME + '/queue.txt'
+CRAWLED_FILE = DIRECTORY_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = args.bots()
 
 queue = Queue()
-crawler(DIRECTORY_NAME, HOME_PAGE)
+crawler(DIRECTORY_NAME, HOME_PAGE, HEADERS)
 
 def create_bot():
 	for _ in range(NUMBER_OF_THREADS):
