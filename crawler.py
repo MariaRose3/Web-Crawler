bae9192c5cd3b3a6cf311eecdda37a315498640a
MariaRose3 <mariapaliyam@gmail.com>
2021-04-10 12:09:14 -0500

    last commit

--- crawler.py
+++ crawler.py
@@ -6,19 +6,17 @@ from file_manage import *
 class crawler:
 	directory_name = ''
 	base_url = ''
-	# domain_name = ''
 	queue_file = ''
 	crawled_file = ''
 
-	def __init__ (self, directory_name, base_url):
+	def __init__ (self, directory_name, base_url, headers):
 		crawler.directory_name = directory_name
 		crawler.base_url = base_url
-		# crawler.domain_name = domain_name
 		crawler.queue_file = crawler.directory_name + '/queue.txt'
 		crawler.crawled_file = crawler.directory_name + '/crawled.txt'
 		crawler.out_of_domain_file = crawler.directory_name + '/out_of_domain.txt'
 		self.boot()
-		self.crawl_page('FirstCrawler', crawler.base_url)
+		self.crawl_page('FirstCrawler', crawler.base_url, headres)
 
 	@staticmethod
 	def boot():
@@ -41,22 +39,7 @@ class crawler:
 	def gather_links(page_url, headers):
 		FindLink(crawler.base_url, page_url, headers)
 		return link_in_domain()
-
-
-		'''html_string = ''
-		try:
-			response = urlopen(page_url)
-			if response.getheader('Content-type') == 'text/html':
-				html_bytes = response.read()
-				html_string = html_bytes.decode('utf-8')
-			finder = FindLink(crawler.base_url, page_url)
-			finder.feed(html_string)
-		except:
-			print('Error')
-			return set()
-		return finder.page_links()
-		'''
-
+		
 	@staticmethod
 	def add_links_to_queue(links):
 		for url in links:
